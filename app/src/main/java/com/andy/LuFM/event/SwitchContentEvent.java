63f24cb8f30a3777e0d7f53d175f519ce54d7beb
wanglu <wanglupublic@163.com>
2016-03-04 09:57:57 +0800

    add click event for more container in homepage

--- app/src/main/java/com/andy/LuFM/event/SwitchContentEvent.java
+++ app/src/main/java/com/andy/LuFM/event/SwitchContentEvent.java
@@ -4,6 +4,9 @@ package com.andy.LuFM.event;
  * 切换fragment视图事件
  */
 public class SwitchContentEvent {
+    public static final String SWITCH_TYPE_CHANNEL_DETAIL = "channeldetail";
+    public static final String SWITCH_TYPE_SPECIAL_TOPIC = "specialtopic";
+    public static final String SWITCH_TYPE_CATEGORY_DETAIL = "category_detail";
     public String type;
     public Object params;
 }
